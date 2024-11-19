d3c01fb90e69ed7eba1d04a1993e125c847bad9a
Faatimah <adebanjofatimo2000@gmail.com>
2024-11-19 19:14:24 +0100

    added nav links to header

--- app/Components/header.tsx
+++ app/Components/header.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import logo from "../../public/next.svg"
+import Link from "next/link";
+
 const Header = () =>{
 return(
     <div>
-        <h1>Header</h1>
+        <nav className="flex justify-between p-4" >
+            <Image src={logo} alt="" className="w-36" />
+            <ul className="flex justify-between gap-20">
+                <li><Link href={"/"}>Home</Link></li>
+                <li><Link href={"/about"}>About</Link></li>
+                <li><Link href={"/blog"}>Blog</Link></li>
+            </ul>
+        </nav>
     </div>
 )
 }
